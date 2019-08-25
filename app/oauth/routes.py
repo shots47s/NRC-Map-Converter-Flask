4c70e0be3b78583302219872317e0ff5158d0c33
Shawn T. Brown <stbrown@msin.ca>
2019-08-25 17:06:07 -0400

    Finished the "Login with google feature

--- app/oauth/routes.py
+++ app/oauth/routes.py
@@ -2,14 +2,17 @@ from flask import flash
 from flask_user import current_user
 from flask_login import login_user
 from flask_dance.contrib.google import make_google_blueprint
-from flask_dance.consumer import oauth_authorized, ouath_error
-from flask_dance consumer.storage.sqla import SQLAlchemyStorage
+from flask_dance.consumer import oauth_authorized, oauth_error
+from flask_dance.consumer.storage.sqla import SQLAlchemyStorage
 from sqlalchemy.orm.exc import NoResultFound
-from app.models import db, User, QAuth
+from app.models import db, User, OAuth
+from datetime import datetime
 
 blueprint = make_google_blueprint(
-  scope=["profile","email"],
-  storage=SQLAlchemyStorage(OAuth, db.session, user=current_user))
+  storage=SQLAlchemyStorage(OAuth, db.session, user=current_user),
+  scope=['https://www.googleapis.com/auth/userinfo.email','openid',
+         'https://www.googleapis.com/auth/userinfo.profile']
+)
 
 
 @oauth_authorized.connect_via(blueprint)
@@ -46,10 +49,45 @@ def google_logged_in(blueprint, token):
       token=token,
     )
 
-  if oauth.user:
-    login_user(oauth.user)
-    flash("Successfully logged in through Google")
-  else:
-    print(dir(google_info))
-    #user = User(email=google_info["email"], first_name=google_info["name"])
+  if current_user.is_anonymous:
+    if oauth.user:
+      print("We are funcking here")
+      login_user(oauth.user)
+      flash("Successfully logged in through Google")
+    else:
+      print("Why is there no oauth")
+      ### No user, so create a user
+      user = User(email=google_info['email'],
+                  first_name=google_info['given_name'],
+                  last_name=google_info['family_name'],
+                  active=True,
+                  email_confirmed_at=datetime.utcnow())
+      oauth.user = user
 
+      db.session.add_all([user,oauth])
+      db.session.commit()
+
+      login_user(user)
+      flash("Successfully signed in with Google.")
+  else:
+    if oauth.user:
+      pass
+
+    else:
+      oauth.user = current_user
+      db.session.add(oauth)
+      db.session.commit()
+      flash("Successfully linked to Google Account")
+
+  return False
+
+
+
+# notify on OAuth provider error
+@oauth_error.connect_via(blueprint)
+def google_error(blueprint,**kwargs):
+  msg = "OAuth error from {name}! ".format(name=blueprint.name)
+  for k,v in kwargs.items():
+    msg += "{} = {}".format(k,str(v))
+  print("msg= {}".format(msg))
+  flash(msg, category="error")
